dbe4d903907869a3a8a78da131558bc031bd65e5
meowseal <hotdogtoiletcleaner@gmail.com>
2025-12-04 12:01:35 -0600

    Made variable data final

--- Main/src/ObjectCircle.java
+++ Main/src/ObjectCircle.java
@@ -1,7 +1,14 @@
 public class ObjectCircle {
-    private double mass;
-    private double radius;
-    private double weight = mass * Math.PI * Math.pow(radius,2);
+    private final double mass;
+    private final double radius;
+    private final double weight;
+
+    // object param constructor
+    public ObjectCircle(double mass, double radius) {
+        this.mass = mass;
+        this.radius = radius;
+        this.weight = mass * Math.PI * Math.pow(radius,2);
+    }
 
     // Return Statements for Object
     public double getMass() {return mass;}  // returns mass
