9e5432b9a3711f0c732d67d6fe57a8b07064c0d3
meowseal <hotdogtoiletcleaner@gmail.com>
2025-12-04 11:58:22 -0600

    Made a simple circle

---
+++ Main/src/ObjectCircle.java
@@ -0,0 +1,11 @@
+public class ObjectCircle {
+    private double mass;
+    private double radius;
+    private double weight = mass * Math.PI * Math.pow(radius,2);
+
+    // Return Statements for Object
+    public double getMass() {return mass;}  // returns mass
+    public double getRadius() {return radius;}  // returns mass
+    public double getWeight() {return weight;}  // returns mass
+
+}
