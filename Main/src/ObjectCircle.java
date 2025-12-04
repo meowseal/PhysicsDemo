aba83e8bcec641c5756c4dd1e345d2f6e71be95e
meowseal <hotdogtoiletcleaner@gmail.com>
2025-12-04 12:21:48 -0600

    Added Positioning

--- Main/src/ObjectCircle.java
+++ Main/src/ObjectCircle.java
@@ -1,19 +1,32 @@
-// Initialize Class
 public class ObjectCircle {
+    // Object Parts
     private final double mass;
     private final double radius;
     private final double weight;
 
+    // Object World Relation
+    public double x;
+    public double y;
+
     // object param constructor
-    public ObjectCircle(double mass, double radius) {
+    public ObjectCircle(double mass, double radius, double x, double y)
+    {
         this.mass = mass;
         this.radius = radius;
-        this.weight = mass * Math.PI * Math.pow(radius,2);
+        this.weight = mass * Math.PI * Math.pow(radius, 2);
+        this.x = x;      // assign to instance variables
+        this.y = y;
     }
 
-    // Return Statements for Object
-    public double getMass() {return mass;}  // returns mass
-    public double getRadius() {return radius;}  // returns mass
-    public double getWeight() {return weight;}  // returns mass
+    // Getters
+    public double getMass() { return mass; }
+    public double getRadius() { return radius; }
+    public double getWeight() { return weight; }
 
+    // Update position
+    public void updatePosition(double newX, double newY)
+    {
+        this.x = newX;
+        this.y = newY;
+    }
 }
