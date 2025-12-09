ecdf8fa2e966b4dcc18e07f86fa5509ac44292f2
meowseal <hotdogtoiletcleaner@gmail.com>
2025-12-09 12:20:51 -0600

    Redoing with string cuz fuh visuals

---
+++ Main/src/libs/helper.java
@@ -0,0 +1,9 @@
+package libs;
+
+public class helper {
+
+    // Printing
+    public void print(Object data) {System.out.print(data);}
+    public void println(Object data) {System.out.println(data);}
+
+}
---
+++ Main/src/libs/lookupDistance.java
@@ -0,0 +1,34 @@
+// package it into the library
+package libs;
+
+// initialize
+public class lookupDistance  {
+    double distanceMultiplier = 0;
+
+    // Set the multiplier
+    public void setMultiplier(double multiply)
+    {
+        distanceMultiplier = multiply;
+    }
+
+    // return distance multiplier for debugging
+    public double getMultiplier() {
+        return distanceMultiplier;
+    }
+
+    public String getChar(double distance)
+
+    {
+
+        // Return shape given distance * multiplier
+        if (distance * distanceMultiplier > 100) {return " ";}
+        else if (distance * distanceMultiplier < 100 && distance * distanceMultiplier >= 80) {return ".";}
+        else if (distance * distanceMultiplier < 80 && distance * distanceMultiplier >= 60) {return "*";}
+        else if (distance * distanceMultiplier < 60 && distance * distanceMultiplier >= 40) {return "%";}
+        else if (distance * distanceMultiplier < 40 && distance * distanceMultiplier >= 20) {return "$";}
+        else if (distance * distanceMultiplier < 20 && distance * distanceMultiplier >= 0) {return "@";}
+        else {return "#";}
+
+    }
+
+}
